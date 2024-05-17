2f746073b167a8841b1b83813250064b81fda0ae
Rohit <rohitpanthari@deliverable.services>
2024-05-17 15:17:43 +0530

    add trademark symbol

--- pages/tech/index.js
+++ pages/tech/index.js
@@ -1,4 +1,4 @@
-import { Button, Container, Layout } from '@/components';
+import {  Container, Layout } from '@/components';
 
 export default function Tech() {
 
@@ -8,11 +8,9 @@ export default function Tech() {
       <div className='mt-[86px]'>
         <div className='relative'>
          <Container>
-            <div className='sectionHeader max-w-[788px] mx-auto text-center flex flex-col justify-center items-center py-24'>
-                <h2 className='mb-4 font-bold text-secondary text-6xl'>Why our tech is exciting</h2>
-                <p className='mb-5 text-base text-gray font-medium'>Environmentally friendly, circular economy, recyclable, 3DP, adaptable (geometrically 
-                    and enzymes target different waste type), retrofittable</p>
-                    <Button variant='secondary' className='rounded-[50px]'>Explore Now →</Button>
+            <div className='sectionHeader max-w-[788px] mx-auto text-center flex flex-col justify-center items-center py-12'>
+                <h2 className='font-bold text-secondary text-4xl'>Power Generation with TankArc<sup>&trade;</sup></h2>
+                
             </div>
             
          </Container>
