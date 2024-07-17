c56f2a012fa6dc9d35c7f2621622f4f95d801eae
Rohit <rohitpanthari@deliverable.services>
2024-07-17 12:34:36 +0530

    add email and password

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -10,8 +10,8 @@ const sendEmailHandler = async (req, res) => {
       port: 587,
       secure: false,
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: 'info@pipelineorganics.co.uk',
+        pass: 'FunkyLatt1_',
       },
     });
 
